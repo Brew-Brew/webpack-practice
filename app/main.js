fae1535f9aade104f7bd51f3718c9b50ff45264d
seungkyu2 <zx6658@naver.com>
2018-04-15 02:16:37 +0900

    웹팩 기본환경 설정 잡기

---
+++ app/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import {render} from 'react-dom';
+import Greeter from './greeter';
+
+render(<Greeter />, document.getElementById('root'));
+
+//ES6모듈 정의를 이용하고 리액트 컴포넌트를 렌더링 하도록 구성
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    devtool : 'eval-source-map',
+    context : __dirname + '/app',      // 모듈들이 존재하는 기준 경로 (필수는 아님 없다면 매번 entry 에 풀경로를 적어줘야함)
+    entry : './main.js',    // 엔트리 파일 위치.
+    output : {                       // output의 엔트리가 배열이면 차례대로 엔트리가 만들어짐
+    path : __dirname + '/public', // 번들 파일의 대상 경로
+    filename : 'bundle.js',
+   },
+   module : {
+
+   rules : [
+     {
+         test : /\.js$/,
+         exclude : /node_modules/,
+         loader : "babel-loader",
+      }
+     ]
+   },
+    devServer : {
+      contentBase : "./public",
+      port : 8080,
+      historyApiFallback : true,
+      inline : true
+    }
+  }
